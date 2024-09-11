eddff47f088a54e6419b11e70aa5265e62d4bdcf
felipedisessa <felipetmds@hotmail.com>
2024-09-11 17:45:18 -0500

    WIP2

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,18 @@
-<img src="{{ asset('images/logo.png') }}" alt="Logo" class="img-fluid" style="width: 200px;">
+<img src="{{ asset('images/logo-dark.png') }}" alt="Logo" class="logo img-fluid">
+
+
+<style>
+    /* CSS padrão (modo escuro) */
+    .logo {
+      content: url('{{ asset('images/logo.png') }}');
+      width: 200px;
+    }
+  
+    /* CSS para modo claro */
+    @media (prefers-color-scheme: light) {
+      .logo {
+        content: url('{{ asset('images/logo-white.png') }}');
+      }
+    }
+  </style>
+  
--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\NoticiasController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get("noticias", [NoticiasController::class, "index"]);
