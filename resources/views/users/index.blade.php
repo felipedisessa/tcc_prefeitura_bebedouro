0a8b648dadf5ce7d51f2a9e8dce1eedb490af277
Felipe Di Sessa <felipetmds@hotmail.com>
2024-09-23 16:54:59 -0300

    fix responsive ui

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -20,7 +20,7 @@
 
                             @if (!$showTrashed && $trashedCount > 0)
                             <a href="{{ route('users.index', ['trashed' => true]) }}"
-                               class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">
+                               class="max-sm:flex max-sm:mt-4 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">
                                 Ver Usuários Desativados
                             </a>
                             @elseif ($showTrashed)
