ea77ae2ebdd4db2d0f8cee9250cd29cc7935d57b
Felipe Di Sessa <felipetmds@hotmail.com>
2024-10-22 10:29:44 -0300

    wip

--- resources/views/noticias/create.blade.php
+++ resources/views/noticias/create.blade.php
@@ -15,14 +15,14 @@
                         <div class="mb-4">
                             <label for="name" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Título</label>
                             <input type="text" name="name" id="name" value="{{ old('name') }}" placeholder="Título da Notícia"
-                                   class="text-white mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+                                   class="dark:text-white text-black mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
                             <div id="nameError" class="text-red-500 text-xs mt-1"></div>
                         </div>
 
                         <div class="mb-4">
                             <label for="description" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Descrição</label>
                             <textarea name="description" id="description" rows="10" placeholder="Escreva o conteúdo da Notícia"
-                                      class="text-white mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">{{ old('description') }}</textarea>
+                                      class="dark:text-white text-black mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">{{ old('description') }}</textarea>
                             <div id="descriptionError" class="text-red-500 text-xs mt-1"></div>
                         </div>
 
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -16,14 +16,14 @@
                         <div class="mb-4">
                             <label for="name" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Nome</label>
                             <input type="text" name="name" id="name" value="{{ $user->name }}" placeholder="Nome do Usuário"
-                                   class="text-white mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
+                                   class="dark:text-white text-black mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
                             <div id="nameError" class="text-red-500 text-xs mt-1"></div>
                         </div>
 
                         <div class="mb-4">
                             <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300">E-mail</label>
                             <input name="email" id="email" type="email" placeholder="exemplo@exemplo.com"
-                                   class="text-white mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                                   class="dark:text-white text-black mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
                                    value="{{ $user->email }}">
                             <div id="emailError" class="text-red-500 text-xs mt-1"></div>
                         </div>
@@ -39,28 +39,14 @@
                         <div class="mb-4">
                             <label for="document" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Documento</label>
                             <input name="document" id="document" type="text" placeholder="Somente Números"
-                                   class="text-white mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                                   class="dark:text-white text-black mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
                                    value="{{ $user->document }}">
                             <div id="documentError" class="text-red-500 text-xs mt-1"></div>
                         </div>
 
-{{--                        <div class="mb-4">--}}
-{{--                            <label for="password" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Senha</label>--}}
-{{--                            <input name="password" id="password" type="password" placeholder="Minimo 8 Caracteres"--}}
-{{--                                   class="text-white mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">--}}
-{{--                            <div id="passwordError" class="text-red-500 text-xs mt-1"></div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="mb-4">--}}
-{{--                            <label for="password_confirmation" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Confirmação de Senha</label>--}}
-{{--                            <input name="password_confirmation" id="password_confirmation" type="password" placeholder="Confirme a Senha"--}}
-{{--                                   class="text-white mt-1 block w-full border-gray-300 dark:border-gray-600 dark:bg-gray-700 rounded-md shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50">--}}
-{{--                            <div id="passwordConfirmationError" class="text-red-500 text-xs mt-1"></div>--}}
-{{--                        </div>--}}
-
                         <div>
                             <button type="submit"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600
+                                    class="dark:text-white text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600
                                 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar Usuário</button>
                         </div>
                     </form>
