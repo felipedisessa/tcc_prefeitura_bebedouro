8fc5a63ffe49ba9435145a5dffe225ce32e341e2
felipedisessa <felipetmds@hotmail.com>
2024-11-05 20:09:14 -0300

    wip

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,7 +16,7 @@
                             <div class="relative w-full">
                                 <input type="text" name="query" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
             focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-            dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Procurar por nome ou e-mail"/>
+            dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Procurar por nome ou e-mail" required/>
                                 @if ($showTrashed)
                                     <input type="hidden" name="trashed" value="true">
                                 @endif
